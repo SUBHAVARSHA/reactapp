26e9d3aca2119dd168abe653f047671f42754305
SUBHAVARSHA <subhavarsha.2003@gmail.com>
2024-01-25 12:10:35 +0530

    varsha

--- src/components/classcomponents/StateComponent.jsx
+++ src/components/classcomponents/StateComponent.jsx
@@ -7,11 +7,18 @@ class stateComponent extends React.Component{
             course:"MERN"
         }
     }
+    changeState=()=> {
+        console.log("Kongu Engineering College")
+        
+    }
     render(){
         return(
             <div>
                 <h1>This is a State Component</h1>
                 <h2>Hello, {this.state.name} This is {this.state.course} class</h2>
+                <h3>I am changing the state of the {this.state.name} to{this.changeState()}</h3>
+                <button onMouseOver={this.changeState}>Click Me to Change the State </button>
+                <button onClick={this.changeState}>Click Me to Change the State </button>
             </div>
         )
     }
