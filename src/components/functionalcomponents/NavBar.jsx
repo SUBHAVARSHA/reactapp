0e44f46e486ad98981f5dfa0b1d56d8721d2e186
SUBHAVARSHA <subhavarsha.2003@gmail.com>
2024-01-27 09:19:45 +0530

    varsha

--- src/components/functionalcomponents/NavBar.jsx
+++ src/components/functionalcomponents/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
             <li><Link to="/about">About</Link></li>
             <li><Link to="/experience">Experience</Link></li>
             <li><Link to="/login">Login</Link></li>
-            <li><Link to="/sign-up">Sign-Up</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
         </ol>
     );
 }
--- src/components/functionalcomponents/SignUp.jsx
+++ src/components/functionalcomponents/SignUp.jsx
@@ -1,13 +1,24 @@
-// SignUp.jsx
-import React from 'react';
-
 const SignUp = () => {
   return (
-    <div>
-      <h2>SignUp Component</h2>
-      {/* Additional content for SignUp */}
-    </div>
-  );
-};
 
+      <div>
+
+          <form>
+              {/* Email:<input type="email"></input><br></br><br></br> */}
+              <br></br><br></br>
+              <label for="Username"><b>Username: </b></label>
+              <input type="text" placeholder="Enter Username" name="Username" required></input><br></br><br></br>
+              <label for="email"><b>Email: </b></label>
+              <input type="text" placeholder3="Enter Email" name="email" required></input><br></br><br></br>
+              <label for="password"><b>Password:</b></label>
+              <input type="password" placeholder="Enter Password" name="password" required></input><br></br><br></br>
+              <label for="repeatPassword"><b>Repeat Password: </b></label>
+              <input type="password" placeholder="Repeat Password" name="repeatPassword" required></input><br></br><br></br>
+              <button type="submit" class="SignUp">SignUp</button>
+              <hr></hr>
+          </form>
+      </div >
+
+  )
+}
 export default SignUp;
